6730b260a32133cc4831d1b88350eeac84545c23
Daniel <dantwq90@gmail.com>
2022-03-28 23:28:59 +0800

    edited post page

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -10,16 +10,16 @@ import PostCard from "../../src/components/Post/PostCard";
 //auth
 import { useAuth } from "../../src/context/AuthContext";
 //styles
-import styles from '../../src/components/Post/Post.module.css'
+import styles from "../../src/components/Post/Post.module.css";
 
 export async function getStaticProps(staticProps) {
   const postId = staticProps.params.id;
   const postData = await fetchPostDataById(postId);
   return {
     props: {
-      postData,
+      postData: postData ? postData : {},
     },
-}
+  };
 }
 
 //get static paths which are paths that can be found
@@ -38,29 +38,38 @@ export async function getStaticPaths() {
   };
 }
 
-const PostPage = ( {postData }) => {
-    const {currentUserProfile} = useAuth()
+const PostPage = ({ postData }) => {
+  const { currentUserProfile } = useAuth();
 
   return (
     <>
-    <Head>
-    <title>{postData.user_displayName ? postData.user_displayName : null}&apos;s Post</title>
-    <meta name="description" content="Post" />
-    <link rel="icon" href="/favicon.ico" />
-    </Head>
+      <Head>
+        <title>
+          {postData.user_displayName ? postData.user_displayName : null}&apos;s
+          Post
+        </title>
+        <meta name="description" content="Post" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className={styles.container}>
         <PostCard
           key={postData.id ? postData.id : null}
           id={postData.id ? postData.id : null}
           userID={postData.user_id ? postData.user_id : null}
-          displayName={postData.user_displayName ? postData.user_displayName : null}
-          profilePhoto={postData.user_profilePhoto ? postData.user_profilePhoto : null}
+          displayName={
+            postData.user_displayName ? postData.user_displayName : null
+          }
+          profilePhoto={
+            postData.user_profilePhoto ? postData.user_profilePhoto : null
+          }
           image={postData.image ? postData.image : null}
           title={postData.title ? postData.title : null}
           description={postData.description ? postData.description : null}
           created_at={postData.created_at ? postData.created_at : null}
           likesCount={postData.likesCount ? postData.likesCount : null}
-          postCommentsCount={postData.commentsCount ? postData.commentsCount : null}
+          postCommentsCount={
+            postData.commentsCount ? postData.commentsCount : null
+          }
           currentUserProfile={currentUserProfile ? currentUserProfile : null}
         />
       </main>
